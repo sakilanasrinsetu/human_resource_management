730a9c31978fce7fd26fd45f188e0f7968472d06
sakilanasrinsetu <setusakilanasrin@gmail.com>
2024-12-23 18:22:30 -0600

    Employee Division

--- employee/views.py
+++ employee/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 
 from employee.filters import *
+from utils.decorators import log_activity
+from utils.permissions import CheckCustomPermission
 from .serializers import *
 from utils.custom_veinlet import CustomViewSet
 from employee.models import *
@@ -32,15 +34,80 @@ class OfficeLocationViewSet(CustomViewSet):
     
 
 class EmployeeDivisionViewSet(CustomViewSet):
-    queryset = EmployeeDivision.objects.all().order_by('name')
-    lookup_field = 'slug'
+    queryset = EmployeeDivision.objects.all()
+    lookup_field = 'pk'
     serializer_class = EmployeeDivisionSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [CheckCustomPermission]
     filter_backends = (
         DjangoFilterBackend,
         filters.OrderingFilter,
     )
     filterset_class = EmployeeDivisionFilter
+    
+    @log_activity
+    def create(self, request, *args, **kwargs): 
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(data=request.data)
+        name = request.data.get('name')
+        employee_division_qs = EmployeeDivision.objects.filter(name=name).last()
+        if employee_division_qs:
+            return ResponseWrapper(error_msg='Employee Division Name Must Be Unique', error_code=400)
+        if serializer.is_valid():
+            division_head = serializer.validated_data.pop('division_head', None)
+            if division_head:
+                employee_qs = EmployeeInformation.objects.filter(
+                    slug = division_head
+                ).last()
+                if not employee_qs:
+                    return ResponseWrapper(error_msg='Division Head Information is NOt Found', error_code=404)
+                
+            serializer.validated_data['created_by'] = self.request.user
+            qs = serializer.save()
+            qs.division_head= employee_qs.user
+            qs.save()
+
+            return ResponseWrapper(data=serializer.data, msg='created', status=200)
+        else:
+            return ResponseWrapper(error_msg=serializer.errors, error_code=400)
+
+    @log_activity
+    def update(self, request, slug, *args, **kwargs): 
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(data=request.data, partial=True)
+        
+        qs = EmployeeDivision.objects.filter(slug=slug).last()
+        if not qs:
+            return ResponseWrapper(error_msg='Employee Division not Found', error_code=404)
+
+        name = request.data.get('name')
+        employee_division_qs = EmployeeDivision.objects.exclude(slug = slug).filter(name=name).last()
+        
+        if employee_division_qs:
+            return ResponseWrapper(error_msg='Employee Division Name Must Be Unique', error_code=400)
+        
+        if serializer.is_valid():
+            division_head = serializer.validated_data.pop('division_head', None)
+            
+            if division_head:
+                employee_qs = EmployeeInformation.objects.filter(
+                    slug = division_head
+                ).last()
+                if not employee_qs:
+                    return ResponseWrapper(error_msg='Employee Information is Not Found', error_code=404)
+                
+           
+            serializer.validated_data['updated_by'] = self.request.user
+            qs = serializer.update(instance=qs, validated_data=serializer.validated_data)
+            
+            if division_head:
+                qs.division_head= employee_qs.user
+                qs.save()
+                
+            serializer = EmployeeDivisionSerializer(qs)
+
+            return ResponseWrapper(data=serializer.data, msg='Updated', status=200)
+        else:
+            return ResponseWrapper(error_msg=serializer.errors, error_code=400)
     
 
 class DepartmentViewSet(CustomViewSet):
